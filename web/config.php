624d220aae8a0b8d0943120d3ff5d9432163b22d
kindlen <42705540+kindlen@users.noreply.github.com>
2018-08-31 05:19:44 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
 define('APIKEY', 'AIzaSyARHJJDFtwb5wcnURwwJA3FQRrM16PF27c');
-define('GJ_CODE', 'JP');
-define('SITE_NAME', '大爷常来玩儿啊');
-define('TITLENAME', 'DouDouTube');
+define('GJ_CODE', 'TW');
+define('SITE_NAME', 'Nor');
+define('TITLENAME', 'nor');
 define('EN2DEKEY', 'lujianbupingyishenghou');
 define('EMAIL', 'xiaofufua@protonmail.com');
 ?>
